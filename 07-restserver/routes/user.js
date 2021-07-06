c7b3ffb84a52bd0a69297961f05e95bd8a4214e3
Gabriela Reynoso <gabriela.reynoso@gmail.com>
2021-07-06 15:37:58 -0600

    Fin seccion 9

--- 07-restserver/routes/user.js
+++ 07-restserver/routes/user.js
@@ -1,4 +1,7 @@
 const {Router} = require('express')
+const {check} = require('express-validator')
+const {validarCampos} = require ('../middlewares/validar-campos')
+const {esRoleValido,emailExiste,ExisteUsuarioId} = require ('../helpers/db-validators')
 
 const {usuariosGet,usuariosPut,usuariosPost,usuariosDelete,usuariosPatch} = require('../controllers/user')
 
@@ -6,11 +9,30 @@ const router = Router();
 
 router.get('/', usuariosGet)
 
-router.put('/:id', usuariosPut)
+router.put('/:id',[
+    check('id', 'No es un ID valido').isMongoId(),
+  //  check('id').custom(ExisteUsuarioId),
+    check('rol').custom(esRoleValido),
+    validarCampos
+]
+, usuariosPut)
 
-router.post('/',usuariosPost)
+router.post('/',[ //acá van los middlewares de validación
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+    check('password','El password es obligatorio y con mas de 6 letras').isLength({min:6}),
+    check('correo','El correo no es válido').isEmail(),
+    check('correo').custom(emailExiste),   
+   // check('rol','No es un rol permitido').not().isIn('ADMIN_ROLE', 'USER_ROLE'),
+    check('rol').custom(esRoleValido),   
+    validarCampos
+],usuariosPost)
 
-router.delete('/',usuariosDelete )
+router.delete('/:id',[
+  check('id', 'No es un ID valido').isMongoId(),
+  check('id').custom(ExisteUsuarioId),  
+  validarCampos
+]
+, usuariosDelete)
 
 router.patch('/', usuariosPatch)
 
